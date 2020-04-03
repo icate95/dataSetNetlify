a531cb1e2b0f14e33b38dfd3188e05063568653d
caterina <ianesellicaterina@gmail.com>
2020-04-03 11:23:41 +0200

    modifica index js e netlify toml

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,11 @@ import App from './App';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+// const client = new ApolloClient({
+//   uri: "http://localhost:4000/graphql"
+// });
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: "/.netlify/functions/graphql"
 });
 
 ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
